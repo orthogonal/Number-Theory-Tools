d75d4f8601f6d86c708c14903edc67beb8f3dfda
Andrew Latham <acl68@case.edu>
2012-01-06 14:25:15 -0600

    The sieve of Eratosthenes works, but only with relatively small (in the million) integers. I need to tamper with the PHP settings to make it work with larger numbers.

--- eratosthenes.php
+++ eratosthenes.php
@@ -1,4 +1,20 @@
 <?php //eratosthenes.php
 	$x = $_POST['eratos_x'];
-	echo "$x";
+	$stop = sqrt($x);
+	$options = array();
+	
+	for ($i = 2; $i <= $x; $i++)
+		array_push($options, $i);
+
+	for ($i = 2; $i < $stop; $i++)
+		for ($j = $i; $j < $x; $j++)
+			if ($options[$j] % $i == 0)
+				$options[$j] = 1;
+	
+	echo "The prime numbers less than or equal to $x are: ";
+	for ($i = 0; $i < $x; $i++)
+		if ($options[$i] != 1)
+			echo "$options[$i], ";
+		
+
 ?>
