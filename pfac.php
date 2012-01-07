e077bfb7ae4d5cece26e8020c72292dea6e9c53f
Andrew Latham <acl68@case.edu>
2012-01-06 19:40:54 -0600

    Changed Euler's Phi Function to have superscripts, and changed the Prime Factorization script to use powers instead of a long multiplication string.

--- pfac.php
+++ pfac.php
@@ -4,16 +4,28 @@
 	
 	$i = 2;
 	$j = 1;
+	$pow = 0;
+	$flag = false;
 	$str = "$x = ";
 	while ($i <= $x){
 		if ($x % $i == 0){
-			if ($j != 1) $str = $str . " * ";
-			$str = $str . "$i";
+			if ($j != 1 && !$flag) $str = $str . " * ";
+			if (!$flag) $str = $str . "$i";
+			$pow++;
 			$j++;
 			$x /= $i;
+			$flag = true;
 		}
-		else $i++;
+		else{
+			if ($pow > 1)
+				$str = $str . "<sup>$pow</sup>";
+			$flag = false;
+			$pow = 0;
+			$i++;
+		}
 	}
+	if ($pow > 1)
+		$str = $str . "<sup>$pow</sup>";
 	
 	if ($str == "$hold = $hold") echo "$hold is prime";
 	else echo "$str";
--- phifunction.php
+++ phifunction.php
@@ -25,16 +25,16 @@
 			
 			$phi *= (pow($i, $options[$i]) - pow($i, ($options[$i] - 1)));
 			
-			echo "$i^$options[$i]";
+			echo "$i<sup>$options[$i]</sup>";
 			if ($i != $x){
 				if ($flag){
-					$str1 = $str1 . " * &#981;($i^$options[$i])";
-					$str2 = $str2 . " * ($i" . (($options[$i] == 1) ? " - 1)" : "^$options[$i] - $i^" . ($options[$i] - 1) . ")");
+					$str1 = $str1 . " * &#981;($i<sup>$options[$i]</sup>)";
+					$str2 = $str2 . " * ($i" . (($options[$i] == 1) ? " - 1)" : "<sup>$options[$i]</sup> - $i<sup>" . ($options[$i] - 1) . "</sup>)");
 					$str3 = $str3 . " * (" . pow($i, $options[$i]) . " - " . pow($i, $options[$i] - 1) . ")";
 				}
 				else{
-					$str1 = "&#981;($x) = &#981;($i^$options[$i])";
-					$str2 = "&#981;($x) = ($i" . (($options[$i] == 1) ? " - 1)" : "^$options[$i] - $i^" . ($options[$i] - 1) . ")");
+					$str1 = "&#981;($x) = &#981;($i<sup>$options[$i]</sup>)";
+					$str2 = "&#981;($x) = ($i" . (($options[$i] == 1) ? " - 1)" : "<sup>$options[$i]</sup> - $i<sup>" . ($options[$i] - 1) . "</sup>)");
 					$str3 = "&#981;($x) = (" . pow($i, $options[$i]) . " - " . pow($i, $options[$i] - 1) . ")";
 					$flag = true;
 				}
