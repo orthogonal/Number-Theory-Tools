a8657e90a307e9e9b5aac2d562c3b091c2bf4a6d
Andrew Latham <acl68@case.edu>
2012-01-11 16:16:50 -0600

    Comments and minor improvements to the exponential congruence and legendre symbol scripts.

--- excong.php
+++ excong.php
@@ -3,9 +3,22 @@
 	$b = $_POST['excong_b'];
 	$m = $_POST['excong_m'];
 	
+	/*The steps in solving an exponential congruence are: 
+		1.  Compute phi(m)
+		2.  Check that gcd(k, phi) = gcd(b, phi) = 1
+		3.  Solve ku - phi(m)v = 1 
+		4.  x^k = (b^u)^k, and b^uk = b from above, so solve x = b^u
+		
+		The reason this works is that ku = 1 + phi(m)v, so b^uk = b(b^phi(m)v).  From Euler's Formula, we know b^phi(m) = 1, so b^phi(m)v = 1.
+	*/
+	
+	//<font face='Symbol'> &#186; </font> is congruence
+	//&#981; is phi
+	
 	echo "x<sup>$k</sup> <font face='Symbol'> &#186; </font> $b mod $m<br /><br />";
 	echo "First, compute &#981;(m)<br />";
 	
+	//Step 1:  Compute phi(m)
 	$options = array();	
 	$temp = $m;
 	$i = 2;
@@ -16,6 +29,7 @@
 		}
 		else $i++;
 	}
+	//$options now an array of the prime factors of $m.  If an index of the array has a value, then it's a prime factor and that value is its power.
 	
 	$phi = 1;
 	$flag = false;	//Flag checks if it is the first factor.
@@ -23,13 +37,19 @@
 	$str2 = "";		//str2 is going to be the second line, which goes through phi(p^k) = p^k - p^(k-1)
 	$str3 = "";		//str3 is the third line, which shows the results of the second and first lines.
 	for ($i = 0; $i <= $m; $i++){	//Check every number.  Only the ones with values in the array will do anything.
-		if ($options[$i] != 0){		
+		if ($options[$i] != 0){		//If it is a prime factor (if it isn't, then the value at the index is just going to be 0)
 			if (!$flag) echo "$m = ";
 			else echo " * ";
+			//If it's not the first factor, then there was another factor before it that it was multiplied by.  Remember $flag determines if it's the first factor.
 			
 			$phi *= (pow($i, $options[$i]) - pow($i, ($options[$i] - 1)));
+			//This is from the formula for phi:  phi(p^k) = p^k - p^(k-1).
+			//Also, phi(mn) = phi(m)phi(n), so to find phi(x), you find phi of all the prime factors of x and multiple those phis together.
 			
 			echo "$i<sup>$options[$i]</sup>";
+			
+			//If $i = $m and something's happening, then that means that the number was prime, so you wouldn't write anything like what's described below.
+			//If $i != $m, then the steps above were executed, so something else involving the strings has to be written.
 			if ($i != $m){
 				if ($flag){
 					$str1 = $str1 . " * &#981;($i<sup>$options[$i]</sup>)";
@@ -55,21 +75,29 @@
 	
 	echo "<br /><br />In order for this method to work, gcd($k, &#981;($m)) and gcd($b, &#981;($m)) must both equal 1.<br />";
 	
+	//Step 2.  Check if gcd(k, phi) = gcd(b, phi) = 1.  
+	//The gcd method returns the gcd of two numbers.
 	if ((gcd($k, $phi) != 1) || (gcd($b, $phi) != 1))
 			exit("$gcd is not 1, so this is unsolveable.");
 	
+	//Step 3.  Solve the diophantine equation.
+	//This part is complicated because PHP doesn't like doing modulus on negative numbers.
+	//The diophantine method returns an array with u and v as the values in the 0 and 1 indices.
 	echo "Next, we want to solve the equation ($k)u - &#981;($m)v = 1 using the Extended Euclidean Algorithm<br /><br />";
 	$diop = diophantine($k, $phi, 1);
 	$u = $diop[0];
 	$v = $diop[1];
 	echo "<br />So the solution is ($k)$u - &#981;($m)$v = 1";
 	
+	//Step 4.  Solve x = b^u through successive squaring.
 	echo "<br />So ($u)$k = 1 + &#981;($m)$v";
 	echo "<br />From Euler's Formula, we know b<sup>&#981;(m)</sup> = $b<sup>&#981;($m)</sup> = 1";
 	echo "<br />So b<sup>uk</sup> = b<sup>1+&#981;(m)v</sup> = b * (b<sup>&#981;(m)</sup>)<sup>v</sup> = b * 1<sup>v</sup> = b";
 	echo "<br />We know x<sup>k</sup> = b, and b<sup>uk</sup> = b, so x = b<sup>u</sup> = $b<sup>$u</sup>";
 	echo "<br />Therefore, we just need to solve x <font face='Symbol'> &#186; </font> $b<sup>$u</sup> mod $m";
 	echo "<br />We can find this through successive squaring.";
+	//The successive method simply outputs the result of successive squaring using the successive.php script.
+	//We want to find x = b^u mod m, which is the same as a^k mod m in the successive squaring script.
 	successive($b, $u, $m);
 	
 	function gcd($x, $y){
@@ -104,14 +132,17 @@
 		$coefficients = array();	
 		$results = array();
 		
+		//If y < x, make y > x.  We want y > x.
 		if ($a_diop < $b_diop){
 			$temp = $b_diop;
 			$b_diop = $a_diop;
 			$a_diop = $temp;
 		}
 		
+		//$a_diop > $b_diop.
 		$gcd = $b_diop;
 	
+		//Basic gcd algorithm
 		while ($r != 0){	
 			$r = ($a_diop % $b_diop);
 			$div = intval($a_diop / $b_diop);
@@ -123,7 +154,9 @@
 			if ($r != 0)	
 				$gcd = $r;
 		}
-	
+		//The gcd method isn't called here because more values than just the gcd are needed in the Extended Euclidean Algorithm.
+		
+		//This next bit is copied from the diophantine.php script up until the end.
 		echo "<br />";
 		if ($z % $gcd != 0)	//The GCD has to divide z, so if it doesn't there is no solution.
 			echo "There is no solution - the gcd of $x and $y does not divide $z";
@@ -150,6 +183,9 @@
 			echo ($z * $gcd) . " = (" . ($coef_x1 * $z) . " + $coef_x2 * k) * " . ($x * $gcd) . " + (" . ($coef_y1 * $z) . " + $coef_y2 * k) * " . ($y * $gcd) . "<br />";
 			$u = $coef_x1 * $gcd;
 			$v = $coef_y1 * $gcd;
+			//This is going to get a solution of ku + phi(m)v = 1.  We want v to be a negative number, and we know that u and v will always be opposite signs.
+			//Therefore, keep adding the k-coefficients to both of them until u is positive, because then v will be negative.
+			//Then it will still be ku + phi(m)v = 1, but v is negative, so we can multiply v by -1 to get ku - phi(m)(-v) = 1.
 			while ($u <= 0){
 				$u += $coef_x2;
 				$v += $coef_y2;
@@ -161,7 +197,7 @@
 	}
 
 	function successive ($a, $k, $m) {
-	
+	//For commentary, see the successive.php script.
 		$kfactors = array();
 		$kpowers = array();
 	
--- legendre.php
+++ legendre.php
@@ -7,7 +7,7 @@
 		
 	//p can't be 2.
 	if ($q == 2) exit ("p can't be 2.");
-	//First test to make sure p ($q) is prime.
+	//First test to make sure p ($q, this is confusing) is an odd prime.
 	for ($i = 2; $i <= sqrt($q); $i++)
 		if ($q % $i == 0) exit("$q isn't prime, it is divisible by $i<br />Remember, p must be an odd prime.");
 	
@@ -15,20 +15,28 @@
 	$legendre = 1;
 	echo "<br />($p / $q)";
 	
+	//When p or q gets to 2 or 1, you can figure it out with the formulas.  You have to flip if q is 2 (see after the while loop).
 	while ($p > 2 && $q > 2){
+		//You can simplify the numerator mod the denominator, i.e. (7/5) = (2/5)
+		//Basically we're going to keep flipping here until the numerator or the demoninator is 2 or 1.
 		$p %= $q;
 		echo "<br /> = " . (($legendre == -1) ? "-" : "") . "($p / $q)";
 		
+		//Next, factor out all the 2s, because we can figure those out.
 		$pow = 0;
 		while ($p % 2 == 0){
 			$pow++;
 			$p /= 2;
 		}
 		
+		//Figure out how the legendre symbol changes due to all the 2s previously factored out.
+		//The rule here is that (2/q) = 1 if q is 1 or 7 mod 8, and -1 if p is 3 or 5 mod 8.
 		if ($pow > 0){
 			echo "<br /> = " . (($legendre == -1) ? "-" : "") . "($p / $q) * (2 / $q)<sup>$pow</sup>";
+			//First of all, if you have an even number of 2-factors, then the legendre symbol won't change, because -1 and 1 to even powers are both 1.
 			if ($pow % 2 == 0)
 				echo "<br /> $pow is even, so = " . (($legendre == -1) ? "-" : "") . "($p / $q)";
+			//If you have an odd number, then check what q is, and if it's 3 or 5 mod 8, then you're multiplying by -1 to an odd power, or -1.
 			else{
 				if (($q % 8 == 1) || ($q % 8 == 7)){
 					echo "<br />$q mod 8 = " . ($q % 8) . " so = " . (($legendre == -1) ? "-" : "") . "($p / $q)";
@@ -39,27 +47,46 @@
 				}
 			}
 		}
+		
+		//Now flip p and q.
+		//The rule here is that (p/q) = (q/p) if either of the numbers is congruent to 1 mod 4, and -(q/p) if they are both congruent to 3 mod 4.
+		//If p is 1, then flipping would be a huge waste of time!
+		if ($p > 1){
+			if ($p % 4 == 1)
+				echo "<br />$p mod 4 = " . ($p % 4) . " so " . (($legendre == -1) ? "-($p / $q) = -($q / $p)" : "($p / $q) = ($q / $p)");
+			else if ($q % 4 == 1)
+				echo "<br />$q mod 4 = " . ($q % 4) . " so " . (($legendre == -1) ? "-($p / $q) = -($q / $p)" : "($p / $q) = ($q / $p)");
+			else{
+				echo "<br />$p mod 4 = " . ($p % 4) . " and $q mod 4 = " . ($q % 4) . " so " . (($legendre == -1) ? "-($p / $q) = ($q / $p)" : "($p / $q) = -($q / $p)");
+				$legendre *= -1;
+			}
 		$temp = $p;
 		$p = $q;
 		$q = $temp;
-		if ($p > 2 && $q > 2){
-			if ($p % 4 == 1)
-				echo "<br />$p mod 4 = " . ($p % 4) . " so " . (($legendre == -1) ? "-($q / $p) = -($p / $q)" : "($q / $p) = ($p / $q)");
-			else if ($q % 4 == 1)
-				echo "<br />$q mod 4 = " . ($q % 4) . " so " . (($legendre == -1) ? "-($q / $p) = -($p / $q)" : "($q / $p) = ($p / $q)");
-			else{
-				echo "<br />$q mod 4 = " . ($q % 4) . " and $p mod 4 = " . ($p % 4) . " so " . (($legendre == -1) ? "-($q / $p) = ($p / $q)" : "($q / $p) = -($p / $q)");
-				$legendre *= -1;
-			}
 		}
 	}
-		
+	
+	//By this point, the loop has exited, so either p or q is <= 2.
+	if ($q == 2 && $p > 2){	//We want (2/q) or (1/q) because that's super easy to figure out.  So if we have (p/2), flip.  
+			if ($p % 4 == 1)
+				echo "<br />$p mod 4 = " . ($p % 4) . " so " . (($legendre == -1) ? "-($p / $q) = -($q / $p)" : "($p / $q) = ($q / $p)");
+			else if ($q % 4 == 1)
+				echo "<br />$q mod 4 = " . ($q % 4) . " so " . (($legendre == -1) ? "-($p / $q) = -($q / $p)" : "($p / $q) = ($q / $p)");
+			else{
+				echo "<br />$p mod 4 = " . ($p % 4) . " and $q mod 4 = " . ($q % 4) . " so " . (($legendre == -1) ? "-($p / $q) = ($q / $p)" : "($p / $q) = -($q / $p)");
+				$legendre *= -1;
+			}
+		$temp = $p;
+		$p = $q;
+		$q = $temp;
+	}
+	//Now we have (2/q) or (1/q).  If it's (1/q), then we do nothing.  If it's (2/q), modify the Legendre symbol one last time.
 	if ($p == 2){
 			if (($q % 8 == 1) || ($q % 8 == 7))
 				$legendre *= 1;
 			else
 				$legendre *= -1;
-		}
+	}
 	
 	echo "<br />So ($original_a / $original_p) = $legendre";
 	if ($legendre == 1)
